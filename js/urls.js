24f04c72041a1ae1b8626fbc8226d44d4c04623c
cwdesautels <carld1989@gmail.com>
2013-08-11 23:50:43 -0400

    forgot url file in previous commit

---
+++ js/urls.js
@@ -0,0 +1,9 @@
+define(function() {
+    return {
+        legalSetUrl: 'http://wizards.com/magic/magazine/article.aspx?x=judge/resources/sfrstandard&print=true',
+        setImgUrl: {
+            prefix: 'http://gatherer.wizards.com/Handlers/Image.ashx?type=symbol&set=',
+            suffix: '&size=small&rarity=C'
+        }
+    }
+});
